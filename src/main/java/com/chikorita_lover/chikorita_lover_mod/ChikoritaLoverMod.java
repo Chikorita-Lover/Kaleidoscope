dc1d8dde298b07c1aa3c71c86ead955f7c58680f
Chikorita Lover <cjgardels@gmail.com>
2022-12-08 23:42:40 -0600

    Initialize project

---
+++ src/main/java/com/chikorita_lover/chikorita_lover_mod/ChikoritaLoverMod.java
@@ -0,0 +1,19 @@
+package com.chikorita_lover.chikorita_lover_mod;
+
+import com.chikorita_lover.chikorita_lover_mod.registry.ModBlocks;
+import com.chikorita_lover.chikorita_lover_mod.registry.ModItems;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ChikoritaLoverMod implements ModInitializer {
+	public static final String MODID = "chikorita_lover";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+	}
+}
