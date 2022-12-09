3c715054367f3daf90ee1f725b5059eb7511f0fb
Chikorita Lover <cjgardels@gmail.com>
2022-12-09 10:13:36 -0600

    Add polished calcite walls
    Assign tags to polished calcite slabs and stairs

--- src/main/java/com/chikorita_lover/chikorita_lover_mod/registry/ModBlocks.java
+++ src/main/java/com/chikorita_lover/chikorita_lover_mod/registry/ModBlocks.java
@@ -11,9 +11,11 @@ public class ModBlocks {
     public static final Block POLISHED_CALCITE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5f, 6f).sounds(BlockSoundGroup.CALCITE));
     public static final Block POLISHED_CALCITE_SLAB = new SlabBlock(FabricBlockSettings.of(Material.STONE).strength(1.5f, 6f).sounds(BlockSoundGroup.CALCITE));
     public static final Block POLISHED_CALCITE_STAIRS = new StairsBlock(POLISHED_CALCITE.getDefaultState(), AbstractBlock.Settings.copy(POLISHED_CALCITE));
+    public static final Block POLISHED_CALCITE_WALL = new WallBlock(AbstractBlock.Settings.copy(POLISHED_CALCITE));
     public static void register(){
         Registry.register(Registry.BLOCK, new Identifier(ChikoritaLoverMod.MODID, "polished_calcite"), POLISHED_CALCITE);
         Registry.register(Registry.BLOCK, new Identifier(ChikoritaLoverMod.MODID, "polished_calcite_slab"), POLISHED_CALCITE_SLAB);
         Registry.register(Registry.BLOCK, new Identifier(ChikoritaLoverMod.MODID, "polished_calcite_stairs"), POLISHED_CALCITE_STAIRS);
+        Registry.register(Registry.BLOCK, new Identifier(ChikoritaLoverMod.MODID, "polished_calcite_wall"), POLISHED_CALCITE_WALL);
     }
 }
--- src/main/java/com/chikorita_lover/chikorita_lover_mod/registry/ModItems.java
+++ src/main/java/com/chikorita_lover/chikorita_lover_mod/registry/ModItems.java
@@ -11,9 +11,11 @@ public class ModItems {
     public static final BlockItem POLISHED_CALCITE = new BlockItem(ModBlocks.POLISHED_CALCITE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
     public static final BlockItem POLISHED_CALCITE_SLAB = new BlockItem(ModBlocks.POLISHED_CALCITE_SLAB, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
     public static final BlockItem POLISHED_CALCITE_STAIRS = new BlockItem(ModBlocks.POLISHED_CALCITE_STAIRS, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
+    public static final BlockItem POLISHED_CALCITE_WALL = new BlockItem(ModBlocks.POLISHED_CALCITE_WALL, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
     public static void register(){
         Registry.register(Registry.ITEM, new Identifier(ChikoritaLoverMod.MODID, "polished_calcite"), POLISHED_CALCITE);
         Registry.register(Registry.ITEM, new Identifier(ChikoritaLoverMod.MODID, "polished_calcite_slab"), POLISHED_CALCITE_SLAB);
         Registry.register(Registry.ITEM, new Identifier(ChikoritaLoverMod.MODID, "polished_calcite_stairs"), POLISHED_CALCITE_STAIRS);
+        Registry.register(Registry.ITEM, new Identifier(ChikoritaLoverMod.MODID, "polished_calcite_wall"), POLISHED_CALCITE_WALL);
     }
 }
