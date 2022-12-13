af7e7a29eb160b6d7e7225ce329cc53d81bed343
Chikorita Lover <cjgardels@gmail.com>
2022-12-13 17:33:31 -0600

    Add recipe data generation
    All recipes are now generated, and old recipes have deleted

--- src/main/java/com/chikorita_lover/chikorita_lover_mod/ChikoritaLoverModDatagen.java
+++ src/main/java/com/chikorita_lover/chikorita_lover_mod/ChikoritaLoverModDatagen.java
@@ -5,19 +5,34 @@ import com.chikorita_lover.chikorita_lover_mod.registry.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
 import net.minecraft.data.client.Models;
 import net.minecraft.data.client.TextureMap;
+import net.minecraft.data.server.RecipeProvider;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.tag.ItemTags;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Consumer;
+
 import static net.minecraft.data.client.BlockStateModelGenerator.createDoorBlockState;
+import static net.minecraft.recipe.Ingredient.fromTag;
 
 public class ChikoritaLoverModDatagen implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         fabricDataGenerator.addProvider(ChikoritaLoverModModelGenerator::new);
+        fabricDataGenerator.addProvider(ChikoritaLoverModRecipeGenerator::new);
     }
 
     private static class ChikoritaLoverModModelGenerator extends FabricModelProvider {
@@ -54,6 +69,49 @@ public class ChikoritaLoverModDatagen implements DataGeneratorEntrypoint {
             itemModelGenerator.register(ModItems.WAXED_EXPOSED_COPPER_DOOR, ModItems.EXPOSED_COPPER_DOOR, Models.GENERATED);
             itemModelGenerator.register(ModItems.WAXED_WEATHERED_COPPER_DOOR, ModItems.WEATHERED_COPPER_DOOR, Models.GENERATED);
             itemModelGenerator.register(ModItems.WAXED_OXIDIZED_COPPER_DOOR, ModItems.OXIDIZED_COPPER_DOOR, Models.GENERATED);
+        }
+    }
+
+    private static class ChikoritaLoverModRecipeGenerator extends FabricRecipeProvider {
+        private ChikoritaLoverModRecipeGenerator(FabricDataGenerator generator) {
+            super(generator);
+        }
+
+        public static void offerStairsRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input) {
+            ShapedRecipeJsonBuilder.create(output, 4).input('#', input).pattern("#  ").pattern("## ").pattern("###").criterion(hasItem(input), conditionsFromItem(input)).offerTo(exporter);
+        }
+
+        public static void offerDoorRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input) {
+            ShapedRecipeJsonBuilder.create(output, 3).input('#', input).pattern("##").pattern("##").pattern("##").criterion(hasItem(input), conditionsFromItem(input)).offerTo(exporter);
+        }
+
+        public static void offerWaxingRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input) {
+            ShapelessRecipeJsonBuilder.create(output).input(input).input(Items.HONEYCOMB).group(output.asItem().toString()).criterion(hasItem(input), conditionsFromItem(input)).offerTo(exporter, output.asItem().toString() + "_from_honeycomb");
+        }
+
+        @Override
+        protected void generateRecipes(Consumer<RecipeJsonProvider> exporter) {
+            ShapedRecipeJsonBuilder.create(ModBlocks.DIRT_BRICKS, 4).input('#', ItemTags.DIRT).pattern("##").pattern("##").criterion(hasItem(Blocks.DIRT), conditionsFromTag(ItemTags.DIRT)).offerTo(exporter);
+            offerSlabRecipe(exporter, ModBlocks.DIRT_BRICK_SLAB, ModBlocks.DIRT_BRICKS);
+            offerStairsRecipe(exporter, ModBlocks.DIRT_BRICK_STAIRS, ModBlocks.DIRT_BRICKS);
+            offerWallRecipe(exporter, ModBlocks.DIRT_BRICK_WALL, ModBlocks.DIRT_BRICKS);
+            
+            offerPolishedStoneRecipe(exporter, ModBlocks.POLISHED_CALCITE, Blocks.CALCITE);
+            offerSlabRecipe(exporter, ModBlocks.POLISHED_CALCITE_SLAB, ModBlocks.POLISHED_CALCITE);
+            offerStairsRecipe(exporter, ModBlocks.POLISHED_CALCITE_STAIRS, ModBlocks.POLISHED_CALCITE);
+            offerWallRecipe(exporter, ModBlocks.POLISHED_CALCITE_WALL, ModBlocks.POLISHED_CALCITE);
+
+            offerStonecuttingRecipe(exporter, ModBlocks.POLISHED_CALCITE, Blocks.CALCITE);
+            offerStonecuttingRecipe(exporter, ModBlocks.POLISHED_CALCITE_SLAB, ModBlocks.POLISHED_CALCITE);
+            offerStonecuttingRecipe(exporter, ModBlocks.POLISHED_CALCITE_STAIRS, ModBlocks.POLISHED_CALCITE);
+            offerStonecuttingRecipe(exporter, ModBlocks.POLISHED_CALCITE_WALL, ModBlocks.POLISHED_CALCITE);
+
+            offerDoorRecipe(exporter, ModBlocks.COPPER_DOOR, Items.COPPER_INGOT);
+
+            offerWaxingRecipe(exporter, ModBlocks.WAXED_COPPER_DOOR, ModBlocks.COPPER_DOOR);
+            offerWaxingRecipe(exporter, ModBlocks.WAXED_EXPOSED_COPPER_DOOR, ModBlocks.EXPOSED_COPPER_DOOR);
+            offerWaxingRecipe(exporter, ModBlocks.WAXED_WEATHERED_COPPER_DOOR, ModBlocks.WEATHERED_COPPER_DOOR);
+            offerWaxingRecipe(exporter, ModBlocks.WAXED_OXIDIZED_COPPER_DOOR, ModBlocks.OXIDIZED_COPPER_DOOR);
         }
     }
 }
