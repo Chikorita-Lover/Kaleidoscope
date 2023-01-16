253b1a6612d6e939b81459773647321ba0ee6cb8
Chikorita Lover <cjgardels@gmail.com>
2023-01-16 00:24:35 -0600

    Add mason trade offers
    Mason villagers may now buy calcite, sell polished calcite, and sell lapis lazuli tiles

--- src/main/java/com/chikorita_lover/chikorita_lover_mod/registry/ModTradeOffers.java
+++ src/main/java/com/chikorita_lover/chikorita_lover_mod/registry/ModTradeOffers.java
@@ -1,6 +1,7 @@
 package com.chikorita_lover.chikorita_lover_mod.registry;
 
 import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+import net.minecraft.block.Blocks;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.entity.Entity;
@@ -15,6 +16,13 @@ import net.minecraft.village.VillagerProfession;
 
 public class ModTradeOffers {
     public static void register() {
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.MASON, 3, factories -> {
+            factories.add(new TradeOffers.BuyForOneEmeraldFactory(Blocks.CALCITE, 16, 16, 20));
+            factories.add(new TradeOffers.SellItemFactory(ModBlocks.POLISHED_CALCITE, 1, 4, 16, 10));
+        });
+        TradeOfferHelper.registerVillagerOffers(VillagerProfession.MASON, 5, factories -> {
+            factories.add(new TradeOffers.SellItemFactory(ModBlocks.LAPIS_LAZULI_TILES, 1, 1, 12, 30));
+        });
         TradeOfferHelper.registerVillagerOffers(VillagerProfession.LIBRARIAN, 5, factories -> {
             factories.add(new IllagerBaneFactory(30));
         });
