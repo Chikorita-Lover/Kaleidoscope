b58d60943379101f8583060021e29c094573e034
Chikorita Lover <cjgardels@gmail.com>
2023-01-24 21:40:24 -0600

    Update ItemMixin.java
    Map is now used to handle stackable items

--- src/main/java/com/chikorita_lover/chikorita_lover_mod/mixin/ItemMixin.java
+++ src/main/java/com/chikorita_lover/chikorita_lover_mod/mixin/ItemMixin.java
@@ -1,22 +1,27 @@
 package com.chikorita_lover.chikorita_lover_mod.mixin;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.LingeringPotionItem;
-import net.minecraft.item.PotionItem;
-import net.minecraft.item.SplashPotionItem;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Map;
+
 @Mixin(Item.class)
 public class ItemMixin {
-    @Inject(method = "getMaxCount", at = @At("RETURN"), cancellable = true)
-    public void getMaxCount(CallbackInfoReturnable<Integer> cir) {
-        Object object = this;
+    private static final Map<ItemConvertible, Integer> MAX_COUNTS = Map.of(
+            Items.POTION, 16
+    );
 
-        if (object instanceof PotionItem && !(object instanceof SplashPotionItem) && !(object instanceof LingeringPotionItem)) {
-            cir.setReturnValue(16);
+    @Inject(method = "getMaxCount", at = @At("RETURN"), cancellable = true)
+    public void getMaxCount(CallbackInfoReturnable<Integer> info) {
+        Item item = Item.class.cast(this);
+
+        if (MAX_COUNTS.containsKey(item)) {
+            info.setReturnValue(MAX_COUNTS.get(item));
         }
     }
 }
