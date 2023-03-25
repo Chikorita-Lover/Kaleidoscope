e389a02422eef3910bfca5e3318bb70882df8478
Chikorita Lover <cjgardels@gmail.com>
2023-03-25 13:07:25 -0500

    Update zombie villager burning
    More professions are now immune to daylight
    All babies now burn in daylight

--- src/main/java/com/chikorita_lover/chikorita_lover_mod/mixin/ZombieVillagerEntityMixin.java
+++ src/main/java/com/chikorita_lover/chikorita_lover_mod/mixin/ZombieVillagerEntityMixin.java
@@ -4,11 +4,14 @@ import net.minecraft.entity.mob.ZombieVillagerEntity;
 import net.minecraft.village.VillagerProfession;
 import org.spongepowered.asm.mixin.Mixin;
 
+import java.util.List;
+
 @Mixin(ZombieVillagerEntity.class)
 public class ZombieVillagerEntityMixin {
+    public List<VillagerProfession> daylightImmuneProfessions = List.of(VillagerProfession.FARMER, VillagerProfession.FISHERMAN, VillagerProfession.FLETCHER, VillagerProfession.LIBRARIAN, VillagerProfession.SHEPHERD);
+
     protected boolean burnsInDaylight() {
         ZombieVillagerEntity zombieVillager = ZombieVillagerEntity.class.cast(this);
-        VillagerProfession profession = zombieVillager.getVillagerData().getProfession();
-        return profession != VillagerProfession.FARMER && profession != VillagerProfession.FISHERMAN;
+        return !daylightImmuneProfessions.contains(zombieVillager.getVillagerData().getProfession()) || zombieVillager.isBaby();
     }
 }
