05002b05e732607b9ef6db135e709a081929dd5b
Chikorita Lover <cjgardels@gmail.com>
2023-04-10 00:05:39 -0500

    Update cake slice
    Cake slices are now considered snacks

--- src/main/java/com/chikoritalover/kaleidoscope/registry/ModFoodComponents.java
+++ src/main/java/com/chikoritalover/kaleidoscope/registry/ModFoodComponents.java
@@ -3,5 +3,5 @@ package com.chikoritalover.kaleidoscope.registry;
 import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
-    public static final FoodComponent CAKE_SLICE = (new FoodComponent.Builder()).hunger(2).saturationModifier(0.1F).build();
+    public static final FoodComponent CAKE_SLICE = (new FoodComponent.Builder()).hunger(2).saturationModifier(0.1F).snack().build();
 }
