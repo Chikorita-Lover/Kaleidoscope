306ad2f2e9e35580b2e33e35dbe075240646a103
Chikorita Lover <cjgardels@gmail.com>
2023-05-26 09:47:09 -0500

    Fireproof blaze rods and blaze powder

--- src/main/java/com/chikoritalover/kaleidoscope/mixin/ItemMixin.java
+++ src/main/java/com/chikoritalover/kaleidoscope/mixin/ItemMixin.java
@@ -2,15 +2,19 @@ package com.chikoritalover.kaleidoscope.mixin;
 
 import com.chikoritalover.kaleidoscope.registry.MaxItemCountRegistry;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.item.MusicDiscItem;
 import net.minecraft.item.StewItem;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Item.class)
-public class ItemMixin {
+public abstract class ItemMixin {
+    @Shadow public abstract Item asItem();
+
     @Inject(method = "getMaxCount", at = @At("RETURN"), cancellable = true)
     private void getMaxCount(CallbackInfoReturnable<Integer> info) {
         Item item = Item.class.cast(this);
@@ -23,6 +27,13 @@ public class ItemMixin {
         }
         if (item instanceof StewItem) {
             info.setReturnValue(16);
+        }
+    }
+
+    @Inject(method = "isFireproof", at = @At("HEAD"), cancellable = true)
+    public void isFireproof(CallbackInfoReturnable<Boolean> callbackInfo) {
+        if (this.asItem() == Items.BLAZE_ROD || this.asItem() == Items.BLAZE_POWDER) {
+            callbackInfo.setReturnValue(true);
         }
     }
 }
