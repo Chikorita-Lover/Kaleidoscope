a9c1d6a1a5e8b6ba00e3f6bc227eb38c91d13367
Chikorita Lover <cjgardels@gmail.com>
2024-06-02 21:19:18 -0500

    Update item rarities

--- src/main/java/net/chikorita_lover/kaleidoscope/mixin/item/ItemStackMixin.java
+++ src/main/java/net/chikorita_lover/kaleidoscope/mixin/item/ItemStackMixin.java
@@ -1,11 +1,16 @@
 package net.chikorita_lover.kaleidoscope.mixin.item;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.chikorita_lover.kaleidoscope.registry.RarityRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Rarity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Optional;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
@@ -15,5 +20,11 @@ public abstract class ItemStackMixin {
     @ModifyReturnValue(method = "getMaxCount", at = @At("RETURN"))
     private int getMaxCount(int maxCount) {
         return this.getItem().getMaxCount();
+    }
+
+    @ModifyExpressionValue(method = "getRarity", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getOrDefault(Lnet/minecraft/component/DataComponentType;Ljava/lang/Object;)Ljava/lang/Object;"))
+    private Object getRarity(Object rarity) {
+        Optional<Rarity> optional = RarityRegistry.getRarity(this.getItem());
+        return optional.orElse((Rarity) rarity);
     }
 }
---
+++ src/main/java/net/chikorita_lover/kaleidoscope/registry/RarityRegistry.java
@@ -0,0 +1,58 @@
+package net.chikorita_lover.kaleidoscope.registry;
+
+import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.util.Rarity;
+
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class RarityRegistry {
+    private static final HashMap<Item, Rarity> ITEM_TO_RARITY_MAP = new HashMap<>();
+    private static boolean registered;
+
+    public static Optional<Rarity> getRarity(Item item) {
+        if (!registered) {
+            registerPredicates();
+        }
+        return Optional.ofNullable(ITEM_TO_RARITY_MAP.getOrDefault(item, null));
+    }
+
+    private static void registerPredicates() {
+        ITEM_TO_RARITY_MAP.put(Items.GOLDEN_APPLE, Rarity.UNCOMMON);
+        ITEM_TO_RARITY_MAP.put(Items.OMINOUS_BOTTLE, Rarity.UNCOMMON);
+        ITEM_TO_RARITY_MAP.put(Items.NETHERITE_INGOT, Rarity.UNCOMMON);
+        ITEM_TO_RARITY_MAP.put(Items.NAUTILUS_SHELL, Rarity.UNCOMMON);
+        ITEM_TO_RARITY_MAP.put(Items.ENDER_EYE, Rarity.UNCOMMON);
+        ITEM_TO_RARITY_MAP.put(Items.ECHO_SHARD, Rarity.UNCOMMON);
+        ITEM_TO_RARITY_MAP.put(Items.HEAVY_CORE, Rarity.UNCOMMON);
+        ITEM_TO_RARITY_MAP.put(Items.ENDER_CHEST, Rarity.RARE);
+        ITEM_TO_RARITY_MAP.put(KaleidoscopeItems.NETHERITE_SHEARS, Rarity.RARE);
+        ITEM_TO_RARITY_MAP.put(Items.RECOVERY_COMPASS, Rarity.RARE);
+        ITEM_TO_RARITY_MAP.put(Items.TRIDENT, Rarity.RARE);
+        ITEM_TO_RARITY_MAP.put(Items.MACE, Rarity.RARE);
+        ITEM_TO_RARITY_MAP.put(Items.ENCHANTED_GOLDEN_APPLE, Rarity.RARE);
+        ITEM_TO_RARITY_MAP.put(Items.LINGERING_POTION, Rarity.RARE);
+        ITEM_TO_RARITY_MAP.put(Items.MOJANG_BANNER_PATTERN, Rarity.RARE);
+        HashMap<Predicate<Item>, Rarity> map = new HashMap<>();
+        map.put(item -> item.canRepair(new ItemStack(item), new ItemStack(Items.NETHERITE_INGOT)), Rarity.RARE);
+        map.put(item -> item instanceof ArmorItem armorItem && armorItem.getMaterial() == ArmorMaterials.NETHERITE, Rarity.RARE);
+        map.put(item -> item instanceof DiscFragmentItem, Rarity.UNCOMMON);
+        map.put(item -> item.getRegistryEntry().isIn(ItemTags.DECORATED_POT_SHERDS), Rarity.UNCOMMON);
+        map.put(item -> item instanceof SmithingTemplateItem, Rarity.RARE);
+        for (Item item : Registries.ITEM) {
+            if (ITEM_TO_RARITY_MAP.containsKey(item)) {
+                continue;
+            }
+            for (var entry : map.entrySet()) {
+                if (entry.getKey().test(item)) {
+                    ITEM_TO_RARITY_MAP.put(item, entry.getValue());
+                    break;
+                }
+            }
+        }
+        registered = true;
+    }
+}
